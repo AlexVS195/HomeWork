75361932ce138ad6419e68c9df67b73c14b57c07
AlexVS195 <142892792+AlexVS195@users.noreply.github.com>
2023-10-16 20:40:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from pathlib import Path
 import sys
+from normalize import normalize
 from file_parser import process_folder
 
+
+
+def main():
+    # Запитуємо у користувача шлях до директорії для сортування
+    folder_path = input("Введіть шлях до директорії для сортування: ")
+    folder_path = Path(folder_path)
+
+    # Перевіряємо, чи існує директорія
+    if folder_path.exists() and folder_path.is_dir():
+        # Запускаємо функцію сортування та передаємо шлях до директорії
+        process_folder(folder_path)
+        print("Сортування завершено.")
+    else:
+        print("Директорія не існує. Перевірте введений шлях.")
+
 if __name__ == "__main__":
-    folder_path = Path(sys.argv[1])
-    process_folder(folder_path)
+    main()
